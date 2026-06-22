34d314c93da399a2568dfafea677f2bd5b7a4f6a
BigBangTheoryy <akshaypathak001@gmail.com>
2026-06-22 23:14:19 +0530

    modified learning transformers.py file

--- Learning Transformers.py
+++ Learning Transformers.py
@@ -12,11 +12,11 @@ tokenizer = AutoTokenizer.from_pretrained(llm_model)
 #Pipeline
 generator =  pipeline("text-generation", model = model, tokenizer = tokenizer, return_full_text = False, max_new_tokens = 200, do_sample = False)
 
-# prompt = "Write an email apologizing to Sarah for the tragic gardening mishap. Explain how it happened."
-#
-# output = generator(prompt)
-#
-# print(output[0]["generated_text"])
+prompt = "Write an email apologizing to Sarah for the tragic gardening mishap. Explain how it happened."
+
+output = generator(prompt)
+
+print(output[0]["generated_text"])
 
 prompt = "The Capital of France is"
 
